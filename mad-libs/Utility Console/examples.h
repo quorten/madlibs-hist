e82de9a40800c1476dc6bf2195e42df229fcefa4
Andrew Makousky <mako0042@umn.edu>
2008-08-18 18:05:59 -0500

    Save file.

---
+++ mad-libs/Utility Console/examples.h
@@ -0,0 +1,9 @@
+//Dedicated to demonstrating things, of course!
+#ifndef EXAMPLES_H
+#define EXAMPLES_H
+
+int SyntaxCheck(int argc, char* argv[]);
+int ArgumentCheck(int argc, char* argv[]);
+int PointerCheck(int argc, char* argv[]);
+
+#endif
